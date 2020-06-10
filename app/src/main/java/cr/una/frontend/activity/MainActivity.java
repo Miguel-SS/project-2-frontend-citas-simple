4ed1185bfe56e5632e7e2e213c6e406e4467ee6c
AleRodCa <alejandro.rodriguez.carrillo13@gmail.com>
2020-06-10 17:02:09 -0600

    commit

--- app/src/main/java/cr/una/frontend/activity/MainActivity.java
+++ app/src/main/java/cr/una/frontend/activity/MainActivity.java
@@ -4,15 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import cr.una.frontend.R;
 import cr.una.frontend.service.AppointmentService;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
-    AppointmentService appointmentService;
+    private  AppointmentService appointmentService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
 }
