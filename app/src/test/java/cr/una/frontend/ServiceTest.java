cef74969d95a0079da013bca1efe0c05cc5c0698
Miguel <m.sanchez397s@gmail.com>
2020-06-10 22:40:40 -0600

    SIIIUUUU HP

--- app/src/test/java/cr/una/frontend/ServiceTest.java
+++ app/src/test/java/cr/una/frontend/ServiceTest.java
@@ -1,7 +1,10 @@
-package cr.una.frontend.service;
+package cr.una.frontend;
 
 import cr.una.frontend.model.Appointment;
+import cr.una.frontend.service.AppointmentService;
 import cr.una.frontend.utilities.Constants;
+import org.junit.Before;
+import org.junit.Test;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -9,24 +12,40 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import java.io.IOException;
 import java.util.List;
 
-public class pruebamain {
-    public static void main(String[] args) throws IOException {
-        Retrofit retrofit =
-                new Retrofit.Builder()
+public class ServiceTest {
+
+    private Retrofit retrofit;
+    private AppointmentService service;
+
+    @Before
+    public void init() {
+        retrofit = new Retrofit.Builder()
                         .baseUrl(Constants.WS_ENDPOINT)
                         .addConverterFactory(GsonConverterFactory.create())
                         .build();
+        service = retrofit.create(AppointmentService.class);
+    }
 
-        // Create an instance of our GitHub API interface.
-        AppointmentService service = retrofit.create(AppointmentService.class);
-
+    @Test
+    public void findAll() throws IOException {
         // Create a call instance for looking up Retrofit contributors.
         Call<List<Appointment>> call = service.findAll();
 
         // Fetch and print a list of the contributors to the library.
         List<Appointment> appointments = call.execute().body();
+        assert appointments != null;
         for (Appointment appointment : appointments) {
             System.out.println(appointment.getId() + " (" + appointment.getPatient().getName() + ")");
         }
     }
+
+    @Test
+    public void findById() throws IOException {
+        Call<Appointment> call = service.findById(1);
+
+        Appointment appointment = call.execute().body();
+        assert appointment != null;
+        System.out.println(appointment.getId() + " (" + appointment.getPatient().getName() + ")");
+    }
+
 }
